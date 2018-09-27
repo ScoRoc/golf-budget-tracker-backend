2668cca4ed7040f5aa62368b124c18544babd9c7
Scott <scrosehart@gmail.com>
2018-09-27 03:29:13 -0700

    fixed some bugs with fake auto login for testing

--- routes/course.js
+++ routes/course.js
@@ -5,6 +5,18 @@ var mongoose = require('mongoose');
 var User = require('../models/user');
 var Course = require('../models/course');
 
+router.get('/:id', (req, res) => {
+  console.log('user: ', req.params.id);
+  Course.find({userId: req.params.id}, function(err, courses) {
+    if (err) {
+      console.log(err);
+    } else {
+      console.log('courses: ', courses);
+      res.json({courses});
+    }
+  })
+});
+
 router.post('/', (req, res) => {
   const { courseName, notes, user } = req.body;
   Course.create({
@@ -22,62 +34,5 @@ router.post('/', (req, res) => {
     }
   });
 });
-
-router.post('/login', (req, res, next) => {
-  let hashedPass = '';
-  let passwordMatch = false;
-  let email = req.body.email.toLowerCase();
-
-  // Look up the User
-  User.findOne({ email }, function(err, user) {
-    hashedPass = user.password
-    // Compare hashedPass to submitted password
-    passwordMatch = bcrypt.compareSync(req.body.password, hashedPass)
-    if (passwordMatch) {
-      // The passwords match...
-      var token = jwt.sign(user.toObject(), process.env.JWT_SECRET, {
-        expiresIn: 60 * 60 * 24 // expires in 24 hours
-      })
-      res.json({user, token})
-    } else {
-      console.log("Passwords don't match")
-      res.status(401).json({
-        error: true,
-        message: 'Email or password is incorrect'
-      })
-    }
-  })
-})
-
-router.post('/signup', (req, res, next) => {
-  let email = req.body.email.toLowerCase();
-  User.findOne({ email }, function(err, user) {
-    if (user) {
-      res.redirect('/auth/signup')
-    } else {
-      User.create({
-        name: req.body.name,
-        email,
-        password: req.body.password
-      }, function(err, user) {
-        if (err) {
-          console.log("GOT AN ERROR CREATING THE USER")
-          console.log(err)
-          res.send(err)
-        } else {
-          console.log("JUST ABOUT TO SIGN THE TOKEN")
-          var token = jwt.sign(user.toObject(), process.env.JWT_SECRET, {
-            expiresIn: 60 * 60 * 24
-          })
-          console.log("user: ", user)
-          console.log("token: ", token)
-          res.json({user, token})
-        }
-      })
-    }
-  })
-})
-
-
 
 module.exports = router;
