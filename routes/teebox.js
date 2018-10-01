c3b80cfdfd2f64304da658af9f796c2716b002b1
Scott <scrosehart@gmail.com>
2018-09-30 21:33:54 -0700

    added PUT /teebox

--- routes/teebox.js
+++ routes/teebox.js
@@ -3,6 +3,7 @@ var express = require('express');
 var router = express.Router();
 var mongoose = require('mongoose');
 var Course = require('../models/course');
+var Teebox = require('../models/teebox');
 
 // router.get('/:id', (req, res) => {
 //   Course.find({userId: req.params.id}, function(err, courses) {
@@ -14,37 +15,38 @@ var Course = require('../models/course');
 //   })
 // });
 
-router.post('/', (req, res) => {
-  const { course, date, score, price, notes, user } = req.body;
-  Round.create({
-    courseId: course._id,
-    date,
-    score,
-    price,
-    notes,
-    userId: user._id
-  }, function(err, newRound) {
-    if (err) {
-      console.log("GOT AN ERROR CREATING THE COURSE")
-      console.log(err)
-      res.send(err)
-    } else {
-      console.log('newRound: ', newRound);
-      res.json({newRound});
-    }
-  });
-});
-
-// router.put('/', (req, res) => {
-//   const { courseName, notes, courseId } = req.body;
-//   Course.findById(courseId, (err, course) => {
-//     course.courseName = courseName;
-//     course.notes = notes;
-//     course.save((err, updatedCourse) => {
-//       res.json({updatedCourse});
-//     });
+// router.post('/', (req, res) => {
+//   const { course, date, score, price, notes, user } = req.body;
+//   Round.create({
+//     courseId: course._id,
+//     date,
+//     score,
+//     price,
+//     notes,
+//     userId: user._id
+//   }, function(err, newRound) {
+//     if (err) {
+//       console.log("GOT AN ERROR CREATING THE COURSE")
+//       console.log(err)
+//       res.send(err)
+//     } else {
+//       console.log('newRound: ', newRound);
+//       res.json({newRound});
+//     }
 //   });
 // });
+
+router.put('/', (req, res) => {
+  const { name, rating, slope, id } = req.body;
+  Teebox.findById(id, (err, teebox) => {
+    teebox.name = name;
+    teebox.rating = rating;
+    teebox.slope = slope;
+    teebox.save((err, updatedTeebox) => {
+      res.json({updatedTeebox});
+    });
+  });
+});
 
 // router.delete('/', (req, res) => {
 //   Course.findByIdAndRemove(req.body.courseId, function(err) {
