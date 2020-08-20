6c484b38d0a697b9528be5aa67e1789dc8373208
Scott <scrosehart@gmail.com>
2020-08-19 19:06:41 -0700

    still trying to fix mongo atlas....

--- server.js
+++ server.js
@@ -15,7 +15,7 @@ var mongoose = require('mongoose');
 // }); // for heroku NEW using Mongo ATlas
 const MongoClient = require('mongodb').MongoClient;
 const uri =
-  'mongodb+srv://heroku_nxw7hcq0:<password>@my-golf-tracker-prod-01.w059u.mongodb.net/<dbname>?retryWrites=true&w=majority';
+  'mongodb+srv://heroku_nxw7hcq0:<password>@my-golf-tracker-prod-01.w059u.mongodb.net/heroku_nxw7hcq0?retryWrites=true&w=majority';
 const client = new MongoClient(uri, { useNewUrlParser: true });
 client.connect(err => {
   const collection = client.db('test').collection('devices');
