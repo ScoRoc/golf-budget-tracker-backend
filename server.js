f5b8722723f3579d1c04efadcf2f7593e7c9da3c
Scott <scrosehart@gmail.com>
2020-08-19 19:28:38 -0700

    trying with mongo atlas again

--- server.js
+++ server.js
@@ -13,17 +13,18 @@ var mongoose = require('mongoose');
 //   useNewUrlParser: true,
 //   useCreateIndex: true,
 // }); // for heroku NEW using Mongo ATlas
-const MongoClient = require('mongodb').MongoClient;
-const uri =
-  'mongodb+srv://heroku_nxw7hcq0:6ubst35dt162gqnjab0rfao9r2@my-golf-tracker-prod-01.w059u.mongodb.net/heroku_nxw7hcq0?retryWrites=true&w=majority';
-// const uri = 'mongodb://heroku_nxw7hcq0:<password>@my-golf-tracker-prod-01-shard-00-00.w059u.mongodb.net:27017,my-golf-tracker-prod-01-shard-00-01.w059u.mongodb.net:27017,my-golf-tracker-prod-01-shard-00-02.w059u.mongodb.net:27017/<dbname>?ssl=true&replicaSet=atlas-64v6tu-shard-0&authSource=admin&retryWrites=true&w=majority
-// '
-const client = new MongoClient(uri, { useNewUrlParser: true });
-client.connect(err => {
-  const collection = client.db('test').collection('devices');
-  // perform actions on the collection object
-  client.close();
-});
+
+mongoose.connect(process.env.MONGO_ATLAS_URI);
+
+// const MongoClient = require('mongodb').MongoClient;
+// const uri =
+//   'mongodb+srv://heroku_nxw7hcq0:6ubst35dt162gqnjab0rfao9r2@my-golf-tracker-prod-01.w059u.mongodb.net/heroku_nxw7hcq0?retryWrites=true&w=majority';
+// const client = new MongoClient(uri, { useNewUrlParser: true });
+// client.connect(err => {
+//   const collection = client.db('test').collection('devices');
+//   // perform actions on the collection object
+//   client.close();
+// });
 
 var app = express();
 
